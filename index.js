6476aa10e6573d2e467ea6f2f6478ba04717db64
Eric Christian <ray@barrel.zone>
2017-12-12 04:36:43 -0600

    Test 3

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function getWebhook(guild, channel) {
 let avatar = "";
 
 client.on('ready', () => {
-    console.log('Connected to Discord API...');
+    console.log('Connected to Discord API... :o');
 });
 
 client.on('message', message => {
